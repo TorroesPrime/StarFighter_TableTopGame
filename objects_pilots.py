d0932e02217432f1f91d654e76b00dc32cae667a
TorroesPrime <michael.cividanes2014@gmail.com>
2019-03-08 11:26:32 -0500

    working to impliment JSON storage for objects -MC

--- objects_pilots.py
+++ objects_pilots.py
@@ -1,18 +1,42 @@
 from class_pilot import *
+import json
 #from class_pilot import pilot
 #from class_pilot import faction
-listpilots = []
-pilot_seether = pilot("Seether",faction[2],9,8,9,pilot_abilities["seether"])
-listpilots.append(pilot_seether)
-pilot_firestorm = pilot("Alicia 'Fire Storm' Heinz",faction[1],9,8,9,pilot_abilities["firestorm"])
-listpilots.append(pilot_firestorm)
-pilot_redclaw = pilot("Bakhtosh 'Redclaw' nar Kiranka",faction[4],9,5,6,pilot_abilities["redclaw"])
-listpilots.append(pilot_redclaw)
-pilot_dakhath = pilot("Dakhath 'Deathstroke' nar Caxki",faction[4],9,5,6,pilot_abilities["dakhath"])
-listpilots.append(pilot_dakhath)
-pilot_starkiller = pilot("Bhurak 'Starkkiller' nar Caxki",faction[4],7,8,7,pilot_abilities["starkiller"])
-listpilots.append(pilot_starkiller)
-pilot_khajja = pilot("Khajja 'The Fang' nar Ja'Targk",faction[4],8,8,5,pilot_abilities["khajja"])
-listpilots.append(pilot_khajja)
-for pilot in listpilots:
-    pilot.card()
+
+
+pilots_string = '''
+    {
+        "pilot":[
+            {
+                "name": "Seether",
+                "faction": "faction[2]",
+                "pilotskill":9,
+                "gunneryskill":8,
+                "Psych":9,
+                "ability":"pilot_abilities['seether']"
+            }
+            "pilot":[
+            {
+                "name": "Alicia 'Fire Storm' Heinz",
+                "faction": "faction[1]",
+                "pilotskill":4,
+                "gunneryskill":6,
+                "Psych":6,
+                "ability":"pilot_abilities['firestorm']"
+            }
+            "pilot":[
+            {
+                "name": "Bakhtosh 'Redclaw' nar Kilranka",
+                "faction": "faction[14]",
+                "pilotskill":9,
+                "gunneryskill":5,
+                "Psych":6,
+                "ability":"pilot_abilities['redclaw']"
+            }
+        ]
+    }
+    '''
+with open('pilot_files.json','w') as output:
+    json.dump(pilots_string, output)
+output.close()
+
