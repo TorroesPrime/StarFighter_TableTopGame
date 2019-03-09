f7ea297cd6f6832d841a8ebb16527177e553b8f5
TorroesPrime <michael.cividanes2014@gmail.com>
2019-03-09 11:36:31 -0500

    changed class_dice to be a single method

--- class_dice.py
+++ class_dice.py
@@ -1,7 +1,4 @@
 import random
-class dice:
-    def __init__(self, sides):
-        self.sides = int(sides)
-    def roll(self):
-        result = random.randint(1,self.sides)
-        return result
+def roll(sides):
+    result = random.randint(1,sides)
+    return result
